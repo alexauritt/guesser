8bbe253770165b85b12db9f677598a6affc613ed
Alexander Auritt <alexauritt@gmail.com>
2012-05-16 16:09:58 -0400

    random and back from public to number

--- app/controllers/random_numbers_controller.rb
+++ app/controllers/random_numbers_controller.rb
@@ -1,5 +1,10 @@
 class RandomNumbersController < ApplicationController
+  
   def show
     @number = rand(100) + 1
+    respond_to do |format|
+      format.html
+      format.json { render :json => @number }
+    end
   end
 end
