98d7768bbf6de88c4f4b405544c33bb6b25016fb
Alexander Auritt <alexauritt@gmail.com>
2012-05-16 16:31:02 -0400

    moved public/index to main controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Guesser::Application.routes.draw do
+  root :to => "main#index"
+
   resource :random_number, :only => [:show]
   # The priority is based upon order of creation:
   # first created -> highest priority.
