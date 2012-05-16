31b550edb7a243eb1fdb9284f3ef8f943f5b5220
Alexander Auritt <alexauritt@gmail.com>
2012-05-16 15:53:02 -0400

    basic project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guesser::Application.config.secret_token = '4fc5c1f5d10718e34197d168e69cca937e6cf86c75415577f4dd570a10959f1a574ba184e02b3cdcf8d0dc21a59f56ea9a2e7e9584f48d95098e54c7ed8db6cf'
