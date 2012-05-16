ece4befeb482d7e3506f6de672eaeb6603ea5ded
Alexander Auritt <alexauritt@gmail.com>
2012-05-16 16:36:17 -0400

    backbone initialization

---
+++ app/assets/javascripts/guesser.js
@@ -0,0 +1,12 @@
+window.Guesser = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  init: function() {
+    return alert('Hello from Backbone!');
+  }
+};
+$(document).ready(function() {
+  return Guesser.init();
+});
