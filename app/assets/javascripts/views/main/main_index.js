a1033f5b29e0585c6283be55e0dc978507a292a7
Alexander Auritt <alexauritt@gmail.com>
2012-05-23 10:51:48 -0400

    submit event triggered (and ignored)

--- app/assets/javascripts/views/main/main_index.js
+++ app/assets/javascripts/views/main/main_index.js
@@ -1,6 +1,14 @@
 Guesser.Views.MainIndex = Backbone.View.extend({
+  id: 'main',
+  events: {
+    "submit #new_guess": "createGuess"
+  },
   render: function() {
     $(this.el).html(JST['main/index']({secretNumber: this.model}));
     return this;
+  },
+  createGuess: function(e) {
+    e.preventDefault();
+    console.log('we will create a guess here');
   }
 });
