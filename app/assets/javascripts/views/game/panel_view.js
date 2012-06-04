a4b920f679958cb7653d54d04dfe298c85175b92
Alexander Auritt <alexauritt@gmail.com>
2012-06-04 12:14:01 -0400

    panel changes on end game

--- app/assets/javascripts/views/game/panel_view.js
+++ app/assets/javascripts/views/game/panel_view.js
@@ -2,11 +2,16 @@ Guesser.Views.PanelView = Backbone.View.extend({
   id: 'panel',
   tagName: 'section',
   initialize: function(options) {
-    _.bindAll(this, 'render');
+    _.bindAll(this, 'render', 'displayEndGamePanel');
+    this.model.on("game:over", this.displayEndGamePanel);
   },
   render: function() {
     $(this.el).html(JST['game/panel']({}));
     return this;
+  },
+  displayEndGamePanel: function() {
+    $('#end-game').show();
+    $('#new-guess').hide();
   }
   
 });
